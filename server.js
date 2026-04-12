a332f91a22d6c1bce8d88f319154827d16617c9b
Pratik Kharadhara <pratikkharadhara@gmail.com>
2026-04-12 18:50:40 +0530

    added post get and delete

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const app = require('./src/app.js');
+
 app.listen(3000,()=>{
     console.log('the server is running on port 3000');
 })
--- src/app.js
+++ src/app.js
@@ -3,6 +3,48 @@ const express = require('express')
 
 const app = express();
 
+//creating a notes app
+/* const note ={
+    "title":'first note'
+    "description":'taking notes '
+}
+*/
+//to store all the notes we need a array
+
+//express can't read any file that has been sent through body
+//thats why when we are sending data from post man here it says 'undefined'
+// thats why we need to use a middle ware
+
+app.use(express.json()) //automatically parses incoming JSON request bodies and attaches the resulting JavaScript object to req.body
+
+const notes = [] //empty array to store the notes
+
+//api for taking the notes, so we are posting it
+app.post('/notes',(req,res)=>{ //'/notes' is the endpoint to send the data
+    console.log(req.body); //we are sending the data to req , req.body is to access it 
+    notes.push(req.body);//we are adding the req.body to the notes
+
+res.status(201).json({ //status 201 is for succesfully getting the data
+        message:"note created succesfully"
+})
+
+})
+//delete the note
+app.delete('/notes/:index',(req,res)=>{
+    const index=req.params.index //this will give us the index that we have sent to delete
+        delete notes[index]; //this will delete the particular note
+        res.status(200).json({
+            message:'notes deleted succesfully'
+        })
+    })
+//here we are getting the what we sent so using the get method
+app.get('/notes',(req,res)=>{
+        res.status(200).json({
+            message :"the note is succesfully retrived",
+            notes: notes 
+        })
+})
+
 //export it to the server.js
 module.exports= app;
 
