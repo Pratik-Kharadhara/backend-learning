0dd7e48f9bb9fbf36da2c3ad0bc5f2def2a4ced3
Pratik Kharadhara <pratikkharadhara@gmail.com>
2026-04-12 12:59:36 +0530

    correct folder structure to start the server

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-const express = require('express'); //this line intiliaze the express
-
-const app = express(); //this helps to run the server
-
-app.get("/",(req, res)=>{ // '/' means the initial path/home path 
-    res.send('Welcom to the server');
+const app = require('./src/app.js');
+app.listen(3000,()=>{
+    console.log('the server is running on port 3000');
 })
-
-app.get("/favourites",(req,res)=>{ // when a user request for the  '/favourites' it send the response below  
-    res.send('welcome to the favourite page');
-})
-
-app.listen(3000) //the 3000 is the port number for our localhost server
---
+++ src/app.js
@@ -0,0 +1,8 @@
+//src-> app.js to initiliaze the server
+const express = require('express')
+
+const app = express();
+
+//export it to the server.js
+module.exports= app;
+
